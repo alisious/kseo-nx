2b058cb1f8df791da8b43296ad6b78ac5a8fa258
alisious <alisious@wp.pl>
2015-06-22 15:30:29 +0200

    Startowe

---
+++ kseo-nx/ViewModels/ShellViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace kseo_nx.ViewModels
+{
+    public class ShellViewModel :Conductor<Screen>.Collection.OneActive
+    {
+
+        public ShellViewModel()
+        {
+           BackToStartView();
+        }
+
+        public void BackToStartView()
+        {
+            DisplayName = "KSEO 2.0 - Strona startowa.";
+            var ai = new StartPageViewModel {Parent = this};
+            ActivateItem(ai);
+        }
+
+        public void ShowVerifications()
+        {
+            DisplayName = "KSEO 2.0 - Wyszukiwanie osoby.";
+            var ai = new PersonSearchViewModel() { Parent = this };
+            ActivateItem(ai);
+        }
+
+        public void CloseApplication()
+        {
+            this.TryClose();
+        }
+    }
+}
---
+++ kseo-nx/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace kseo_nx.ViewModels
+{
+    public class StartPageViewModel :Screen
+    {
+        public void Close()
+        {
+            
+        }
+
+        public void CheckPerson()
+        {
+            if (this.Parent!=null)
+                (this.Parent as ShellViewModel).ShowVerifications();
+        }
+    }
+}
